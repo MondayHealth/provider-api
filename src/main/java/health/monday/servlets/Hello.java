da01657f06efb1bdd26a067ec94296521d3aa456
Chris Galardi <christophergalardi@gmail.com>
2018-01-09 14:49:37 -0500

    hello, world.

--- src/main/java/health/monday/servlets/Hello.java
+++ src/main/java/health/monday/servlets/Hello.java
@@ -7,13 +7,13 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-@WebServlet(name = "Hello")
-public class Hello extends HttpServlet {
-    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
-    }
-
-    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
-    }
+@WebServlet(name = "HelloServlet", urlPatterns = {"/hello"}, loadOnStartup = 1)
+public class Hello extends HttpServlet
+{
+	protected void doGet(HttpServletRequest request,
+						 HttpServletResponse response)
+			throws ServletException, IOException
+	{
+		response.getWriter().print("{\"success\": true}");
+	}
 }

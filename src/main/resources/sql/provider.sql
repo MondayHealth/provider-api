9920eec2367568247f3bac797026a336978191fe
Chris Galardi <christophergalardi@gmail.com>
2018-01-27 18:50:28 -0500

    more correct not null query

--- src/main/resources/sql/provider.sql
+++ src/main/resources/sql/provider.sql
@@ -5,14 +5,15 @@ SELECT
   pro.website_url,
   ARRAY(SELECT pc.credential_id
         FROM monday.providers_credentials pc
-        WHERE pc.provider_id = pro.id)                            AS credentials,
+        WHERE pc.provider_id = pro.id)                                AS credentials,
   ARRAY(SELECT ps.specialty_id
         FROM monday.providers_specialties ps
         WHERE ps.provider_id =
-              pro.id)                                             AS specialties,
+              pro.id)                                                 AS specialties,
   ARRAY(SELECT
           addy.formatted || '|' || st_x(addy.point) || '|' || st_y(addy.point)
         FROM monday.providers_addresses pa
           JOIN monday.address addy ON pa.address_id = addy.id
-        WHERE pa.provider_id = pro.id AND addy.formatted <> NULL) AS addresses
+        WHERE pa.provider_id = pro.id AND addy.formatted IS NOT
+                                          NULL)                       AS addresses
 FROM monday.provider pro
